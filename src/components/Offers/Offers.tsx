4e974c3d2fe4a1a4fe0f817a4aff01c22ebe25bb
ojage <hismajestylordkratos@gmail.com>
2023-09-09 23:53:36 +0100

    Changed the pic for beach

--- src/components/Offers/Offers.tsx
+++ src/components/Offers/Offers.tsx
@@ -21,8 +21,17 @@ function Offers() {
           </Box>
           <Image borderRightRadius={7} w="40%" src={ConfHall} />
         </Flex>
-        <Box w="52%" color="white" backgroundSize="cover" backgroundImage={ConfHall2} h="30vh" borderRadius={7} boxShadow="rgba(0, 0, 0, 0.1) 0px 4px 12px;">
-          <Box w="50%" m="1rem">
+        <Box w="52%" color="white" backgroundSize="contain" position="relative" h="30vh" borderRadius={7} boxShadow="rgba(0, 0, 0, 0.1) 0px 4px 12px;">
+          <Box zIndex="-1" pos="absolute" top="0" left="0" h="100%" w="100%" backgroundImage="url(https://img.freepik.com/free-vector/fashion-model-with-summer-outfit_79603-528.jpg?size=626&ext=jpg&ga=GA1.1.513754652.1688231868&semt=ais)" transform="rotateY(180deg)" />
+          <Box
+            background="rgba(21, 232, 207, 0.1)"
+            borderRadius="7"
+            boxShadow="0 4px 30px rgba(0, 0, 0, 0.1)"
+            backdropFilter="blur(5px)"
+            w="50%"
+            p="1rem"
+            h="100%"
+          >
             <Text fontWeight={600}>The beginning of something</Text>
             <Text>Save at least 15% on event spaces nationwide, from halls for birthdays to grand stadiums for concerts</Text>
             <br />
