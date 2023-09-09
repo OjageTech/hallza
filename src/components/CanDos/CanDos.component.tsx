7897759ad8d194fc863e96edf5eac881b88c7dd0
ojage <hismajestylordkratos@gmail.com>
2023-09-09 23:06:49 +0100

    Added the disabled effect for some features

--- src/components/CanDos/CanDos.component.tsx
+++ src/components/CanDos/CanDos.component.tsx
@@ -2,13 +2,9 @@
 /* eslint-disable react/require-default-props */
 import { Link as RouteLink } from 'react-router-dom';
 import {
-  Text, Flex, Image, Grid, GridItem,
+  Text, Flex, Image, Grid, GridItem, Tooltip,
 } from '@chakra-ui/react';
-import {
-  ROUTE,
-  USER_DASHBOARD_ROUTE,
-  IN_DASHBOARD_ROUTE,
-} from '../../routes';
+import { ROUTE, USER_DASHBOARD_ROUTE, IN_DASHBOARD_ROUTE } from '../../routes';
 import Box from '../common/Box';
 import Card from '../common/Card';
 import Badger from '../../layouts/sidebars/UserDashboard/Badger';
@@ -42,8 +38,7 @@ const CanDosData = [
   },
   {
     title: 'Launch your First Hall',
-    subtitle:
-      'Learn Hallza fundamentals and start building with tutorials',
+    subtitle: 'Learn Hallza fundamentals and start building with tutorials',
     img: edgeEditorial,
     newBadge: false,
     color: 'secondary',
@@ -88,22 +83,35 @@ const CanDo = ({
 }: CanDoProps) => (
   <GridItem>
     <Card
+      title={title === 'Hallza Bills' || title === 'Hallza Catering' ? 'Coming soon' : title}
       as={RouteLink}
-      to={path}
+      to={
+          title === 'Hallza Bills' || title === 'Hallza Catering' ? '/' : path
+        }
       onClick={onClick}
       p="2px"
       w="225"
+      boxShadow={
+          title === 'Hallza Bills' || title === 'Hallza Catering'
+            ? 'rgba(17, 17, 26, 0.1) 0px 1px 0px;'
+            : 'rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;'
+        }
       h="224"
     >
-      <Image w={225} h="auto" src={img} />
-      <Flex
-        p={0}
-        justifyContent="left"
-        alignItems="center"
-        gap=".5rem"
-      >
+      <Image
+        w={225}
+        h="auto"
+        src={img}
+        style={{ filter: 'grayscale(100%)' }}
+      />
+
+      <Flex p={0} justifyContent="left" alignItems="center" gap=".5rem">
         <Text
-          color={color}
+          color={
+              title === 'Hallza Bills' || title === 'Hallza Catering'
+                ? 'gainsboro'
+                : color
+            }
           lineHeight={{
             base: '.9rem',
             md: '1.2rem',
@@ -129,6 +137,11 @@ const CanDo = ({
       </Flex>
       <Text
         textAlign="center"
+        color={
+            title === 'Hallza Bills' || title === 'Hallza Catering'
+              ? 'gainsboro'
+              : 'inherit'
+          }
         mt=".5rem"
         fontWeight={300}
         lineHeight="1rem"
@@ -146,10 +159,7 @@ const CanDo = ({
 );
 
 const ProminentCanDos = () => (
-  <Box
-    margin="0 auto"
-    w="95vw"
-  >
+  <Box margin="0 auto" w="95vw">
     <Grid
       mt="1rem"
       templateColumns={{
@@ -190,7 +200,7 @@ const ProminentCanDos = () => (
 );
 
 CanDo.defaultProps = {
-  onClick: () => console.log('canDo-french'),
+  onClick: () => console.log('Default Immediately evoked Function prop'),
 };
 
 export default ProminentCanDos;
