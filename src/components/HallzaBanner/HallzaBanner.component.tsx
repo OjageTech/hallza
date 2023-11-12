7b0fc03f244c63b5c48b7ac17e35ad835405a7be
Salathiel Ojage <salathiel@Salathiels-MacBook-Air.local>
2023-11-12 16:58:24 +0100

    Added

--- src/components/HallzaBanner/HallzaBanner.component.tsx
+++ src/components/HallzaBanner/HallzaBanner.component.tsx
@@ -6,6 +6,28 @@ import {
   IN_DASHBOARD_ROUTE,
 } from '../../routes';
 
+const BannerFeatures = [
+  {
+    path: ROUTE.LOGIN,
+    title: 'Solutions',
+    subtitle: 'View our Solutions library',
+  },
+  {
+    path: ROUTE.LOGIN,
+    title: 'Products',
+    subtitle: 'Checkout the products we offer to help your event',
+  },
+  {
+    path: ROUTE.LOGIN,
+    title: 'Job Opportunities',
+    subtitle: 'Take a look and apply for Hallza gigs',
+  },
+  {
+    path: ROUTE.LOGIN,
+    title: 'Customer Innovation',
+    subtitle: 'See our customer success stories',
+  },
+];
 interface BannerItemI {
   title: string;
   subtitle: string;
@@ -30,26 +52,11 @@ const HallzaBanner = () => (
     justifyContent="space-between"
     textAlign="center"
   >
-    <BannerItem
-      path={ROUTE.LOGIN}
-      title="Solutions"
-      subtitle="View our Hallza Solutions Library"
-    />
-    <BannerItem
-      path={ROUTE.LOGIN}
-      title="Products"
-      subtitle="View our Hallza Solutions Library"
-    />
-    <BannerItem
-      path={ROUTE.LOGIN}
-      title="Solutions"
-      subtitle="View our Hallza Solutions Library"
-    />
-    <BannerItem
-      path={ROUTE.LOGIN}
-      title="Customer Innovation"
-      subtitle="See our Customer success stories"
-    />
+    {
+      BannerFeatures.map((feature) =>
+        <BannerItem path={feature.path} title={feature.title} subtitle={feature.subtitle} />,
+      )
+    }
   </Flex>
 );
 
